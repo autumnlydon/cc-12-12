2e47f365f70d612cd8a752b159226f62e266bb86
Autumn Lydon <autumnlydon@gmail.com>
2024-12-12 10:27:32 -0500

    algorithm

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ALGORITHM: 
+
+make array of all letters
+compare to whats pushed in included array
+return array of letters that do not match up
+
+push whats not in there to an array
+only sort array as last step
+*/

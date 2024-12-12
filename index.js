fb9f7943045f88ae7b85f5d38b45db1821b583b7
Autumn Lydon <autumnlydon@gmail.com>
2024-12-12 10:51:03 -0500

    lil shorter

--- index.js
+++ index.js
@@ -9,39 +9,50 @@ push whats not in there to an array
 only sort array as last step
 */
 
+// const solution = (inputStr) => {
+//   const alpha = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//   ];
+//   const stringArray = inputStr.split("");
+//   const alphaArray = [];
+//   for (let i = 0; i < alpha.length; i++) {
+//     if (!stringArray.includes(alpha[i])) {
+//       alphaArray.push(alpha[i]);
+//     }
+//   }
+//   return alphaArray;
+// };
+
 const solution = (inputStr) => {
-  const alpha = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  const stringArray = inputStr.split("");
+  const alpha = "abcdefghijklmnopqrstuvwxyz";
   const alphaArray = [];
   for (let i = 0; i < alpha.length; i++) {
-    if (!stringArray.includes(alpha[i])) {
+    if (!inputStr.includes(alpha[i])) {
       alphaArray.push(alpha[i]);
     }
   }
